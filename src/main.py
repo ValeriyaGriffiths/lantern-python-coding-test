eca99b30480c7aed66d9c9f54df4c5dab679ed6d
heidi-val <val.griffiths@heidipay.com>
2024-07-11 23:46:24 +0100

    remove async

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ app = FastAPI(debug=settings.debug)
 
 
 @app.post("/company/validate-pdf-data", response_model=DataDiscrepancyCheckerResponse, response_model_by_alias=False)
-async def validate_pdf_company_data(company_name: str, data_file: UploadFile = File(...)):
+def validate_pdf_company_data(company_name: str, data_file: UploadFile = File(...)):
     """
     Take company name and a pdf with company data and validate the data against stored data.
     Returns extracted pdf data, stored data, and list of mismatched fields.
