5adcd5eb0092353aa8f8fefa7aeca642e4e46b8c
heidi-val <val.griffiths@heidipay.com>
2024-07-11 01:34:59 +0100

    add pdf service api

---
+++ src/pdf_service_api.py
@@ -0,0 +1,28 @@
+from pdf_service import PdfService
+from src.data_layer import parse_company_data
+from src.models import CompanyData
+
+
+COMPANY_NAME_FILE_PATH_MAPPING = {
+    "HealthInc": "assets/healthinc.pdf",
+    "RetailCo": "assets/retailco.pdf",
+    "FinanceLLC": "assets/financellc.pdf",
+}
+
+
+def extract_and_parse_pdf_data(company_name: str, pdf_file) -> CompanyData:
+    """
+    Calls PdfService to extract data from pdf file corresponding to comp
+
+    :param company_name: name of company
+    :return: CompanyData object containing extracted data
+    """
+
+    # realistically, this might be replaced by a table containing records of uploaded company files.
+    file_path = COMPANY_NAME_FILE_PATH_MAPPING[company_name]
+
+    # a real service might take a token in the request header, but using hard-coded key here for mocked service.
+    pdfs = PdfService(key="TEST_KEY")
+    return parse_company_data(pdfs.extract(file_path=file_path))
+
+
